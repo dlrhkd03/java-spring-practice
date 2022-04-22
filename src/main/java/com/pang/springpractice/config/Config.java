ce7a31302d688207f7103196538e68050d5e5fcd
pang <slsl01@naver.com>
2022-04-22 14:08:49 +0900

    Config #1 - spring configuration
    기초 설정
    루트 패키지 이하에 빈 등록

---
+++ src/main/java/com/pang/springpractice/config/Config.java
@@ -0,0 +1,10 @@
+package com.pang.springpractice.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan("com.pang.springpractice")
+@Configuration
+public class Config {
+
+}
